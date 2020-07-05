29644019b60e88f5c155e842812e5d548aaabbca
k <kyungminkim7@gmail.com>
2020-07-04 22:01:17 -0500

    Adjusted rover to reflect new ntwk interface

--- apps/rover.cpp
+++ apps/rover.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <network/Node.h>
+#include <network/Rate.h>
 #include <network/TcpPublisher.h>
 
 #include <opencv2/core.hpp>
@@ -12,7 +13,7 @@ const unsigned short IMG_PORT = 50000;
 int main(int argc, char *argv[]) {
     // Initialize ntwk publishers/subscribers
     ntwk::Node node;
-    auto imgPublisher = node.advertise(IMG_PORT, 5, ntwk::Compression::JPEG);
+    auto imgPublisher = node.advertise(IMG_PORT, ntwk::Compression::JPEG);
 
     // Initialize camera
     cv::VideoCapture cam(0);
@@ -24,12 +25,13 @@ int main(int argc, char *argv[]) {
     while(true) {
         cv::Mat img;
         if (cam.read(img)) {
+            cv::resize(img, img, cv::Size(), 0.5f, 0.5f);
             cv::cvtColor(img, img, cv::COLOR_BGR2RGB);
             cv::flip(img, img, 0);
             imgPublisher->publishImage(img.cols, img.rows, 3, img.data);
         }
 
-        node.update();
+        node.runOnce();
     }
 
     return 0;
