80005d9415aeda0701698412b36975b554f43c21
Ubuntu <ubuntu@ubuntu.attlocal.net>
2021-11-10 17:25:56 +0000

    Squashed 'extern/network/' changes from a14c5a2..0b1c412
    0b1c412 Fixed dangling socket pointer bug in TcpPublisher
    afae8f0 Improved TcpPublisher to allow publishing msgs from multiple threads
    94c43f5 Added ThreadGuard class
    3931c39 Modified Node constructor to accept a shared_ptr<asio::io_context>
    6e85adb Updated Node class to use Thread class
    30af30a Changed ThreadGuard class to Thread
    85aa09b Updated Node to use ThreadGuard
    1df3dfb Introduced ThreadGuard class
    5a59b87 Commented out libjpeg-turbo CMakeLists INSTALL instructions
    8a9320e Added support for JPEG
    138d279 Merge commit 'fd75654aa31d1fb358094bd8594edcc39f583b61' as 'extern/libjpeg-turbo'
    fd75654 Squashed 'extern/libjpeg-turbo/' content from commit d401d6251
    9775c14 Fixed Node::subscribe() to accept the right type of handler to match TcpSubscriber::subscribe()
    730268a Fixed TcpPublisher to delete 1st matching socket on error rather than iterating through the entire list of connected sockets
    f837e34 Corrected TcpPublisher::publish() to not block on trying to send msgs
    ff6c819 Changed TcpPublisher to check and make sure sockets are available before publishing
    aed8702 Added Image.cpp
    fa166bb Added helper functions for converting btwn Images and msg buffers
    4fee6ed Changed TcpSubscriber msg handler type to receive rvalue references of the msg buffer unique_ptr
    bed3328 Fixed top-lvl CMakeLists.txt to remove libjpeg dependency
    386bd0b Removed libjpeg dependency
    4fd7f8e Changed TcpSubscriber::receiveMsg and ::postMsgHandlingTask to accept rvalue refs to subscriber
    d359e54 Fixed TcpSubscriber implementation to handle potential race conditions in receiving an entire msg
    7733887 Fixed race condition in TcpPublisher sending multiple msgs at once
    28732a8 Centralized MsgTypeId into a separate scoped enum
    6811202 Refactored the following Msg types: Joy into Joystick and MessageControl into MsgCtrl
    a94cf07 Modified network message types to have a flat hierarchy for simplicity
    e2da3b5 Modified TcpPublisher and TcpSubscriber to allow sending multiple msgs over 1 connection.
    b6ed37e Changed Node to be the ultimate Facade in keeping track of multiple subscribers (to potentially different machines) as well as it's own publisher
    30a58b3 Removed unnecessary struct Image forward declaration in TcpSubscriber.h
    24dbb3d Introduced TcpSubscriber::MsgPtr alias
    e34e3f4 Changed Node::runOnce() to poll rather than just poll once
    667038c Modified TcpSubscriber to synchronize msg received callbacks via posting to asio::io_context rather than locking mutexes for efficiency
    29ae909 Removed unnecessary mutex on TcpSubscriber socket
    b0c5c51 Removed unnecessary TcpPublisher dependency on vector
    7b55ba8 Fixed Rate to use std::this_thread::sleep_until() to sleep for a more accurate period
    12246ef Fixed TcpPublisher.cpp formatting
    8d1144e Changed TcpPublisher::listenForConnections to just not save an erroneous connection rather than throwing an exception and exiting
    c64667d Moved struct Socket from TcpPublisher header to source file
    d17a60b Removed Compression and TcpPublisher and TcpSubscriber interface for transferring Images
    ce5ae39 Modified TcpPublisher::removeSocket() to use std::list remove functionality
    35959ea Merge commit '69e1d677488e6107818e1beaa4bf9f1af6c469cc' into dev
    69e1d67 Squashed 'extern/flatbuffers/' changes from 712866d57..9a4ca2764
    73d425b Merge commit 'ae7875b70ea11df463c7434921fee41450592f67' into dev
    ae7875b Squashed 'extern/asio/' changes from a5c5077e..55435a0e
    17cc6f6 Disabled build of libjpeg-turbo shared library
    77963ba Changed compression strategy in TcpPublisher and TcpSubscriber to a template policy
    
    git-subtree-dir: extern/network
    git-subtree-split: 0b1c4126cb8eb180a97b1eee1ad9077158c2394f

--- include/network/TcpPublisher.h
+++ include/network/TcpPublisher.h
@@ -2,63 +2,39 @@
 
 #include <list>
 #include <memory>
-#include <type_traits>
-#include <vector>
 
 #include <asio/ip/tcp.hpp>
 #include <asio/io_context.hpp>
-#include <std_msgs/Header_generated.h>
-#include <std_msgs/MessageControl_generated.h>
+#include <flatbuffers/flatbuffers.h>
+
+#include "MsgTypeId.h"
 
 namespace ntwk {
 
-template<typename CompressionStrategy>
-class TcpPublisher : public std::enable_shared_from_this<TcpPublisher<CompressionStrategy>> {
+class TcpPublisher : public std::enable_shared_from_this<TcpPublisher> {
+private:
+    struct Socket;
+    using SocketPtr = std::shared_ptr<Socket>;
+    using PublisherPtr = std::shared_ptr<ntwk::TcpPublisher>;
+
 public:
     static std::shared_ptr<TcpPublisher> create(asio::io_context &publisherContext,
-                                                unsigned short port,
-                                                CompressionStrategy compressionStrategy);
+                                                unsigned short port);
 
-    void publish(std::shared_ptr<flatbuffers::DetachedBuffer> msg);
-    void publish(unsigned int width, unsigned int height, uint8_t channels, const uint8_t data[]);
+    void publish(MsgTypeId msgTypeId, std::shared_ptr<flatbuffers::DetachedBuffer> msg);
 
 private:
-    struct Socket {
-        std::unique_ptr<asio::ip::tcp::socket> socket;
-        bool readyToWrite;
-
-        explicit Socket(std::unique_ptr<asio::ip::tcp::socket> socket) :
-            socket(std::move(socket)), readyToWrite(true){}
-    };
-
-    TcpPublisher(asio::io_context &publisherContext, unsigned short port,
-                 CompressionStrategy compressionStrategy);
+    TcpPublisher(asio::io_context &publisherContext, unsigned short port);
 
     void listenForConnections();
-    void removeSocket(Socket *socket);
-
-    static void sendMsgHeader(std::shared_ptr<ntwk::TcpPublisher<CompressionStrategy>> publisher, Socket *socket,
-                              std::shared_ptr<const std_msgs::Header> msgHeader,
-                              std::shared_ptr<const flatbuffers::DetachedBuffer> msg,
-                              unsigned int totalMsgHeaderBytesTransferred);
-
-    static void sendMsg(std::shared_ptr<ntwk::TcpPublisher<CompressionStrategy>> publisher, Socket *socket,
-                        std::shared_ptr<const flatbuffers::DetachedBuffer> msg,
-                        unsigned int totalMsgBytesTransferred);
-
-    static void receiveMsgControl(std::shared_ptr<TcpPublisher<CompressionStrategy>> publisher, Socket *socket,
-                                  std::unique_ptr<std_msgs::MessageControl> msgCtrl,
-                                  unsigned int totalMsgCtrlBytesReceived);
+    static void sendMsg(PublisherPtr &&publisher, SocketPtr &&socket,
+                        ntwk::MsgTypeId msgTypeId);
 
 private:
     asio::io_context &publisherContext;
     asio::ip::tcp::acceptor socketAcceptor;
 
-    std::list<Socket> connectedSockets;
-    std::conditional_t<std::is_function<CompressionStrategy>::value,
-                       std::add_pointer_t<CompressionStrategy>, CompressionStrategy> compressionStrategy;
+    std::list<SocketPtr> connectedSockets;
 };
 
 } // namespace ntwk
-
-#include "TcpPublisher_impl.h"
--- src/Node.cpp
+++ src/Node.cpp
@@ -1,28 +1,44 @@
 #include <network/Node.h>
 
+#include <network/TcpPublisher.h>
+#include <network/TcpSubscriber.h>
+
 namespace ntwk {
 
-Node::Node() : mainContext(), tasksContext(),
-    tasksThread([this]{
-        auto work = asio::make_work_guard(this->tasksContext);
-        this->tasksContext.run();
-    }) { }
+Node::Node(ContextPtr context) :
+    mainContext(std::move(context)),
+    ntwkContext(std::make_shared<asio::io_context>()),
+    ntwkThread(ntwkContext) { }
 
 Node::~Node() {
-    this->tasksContext.stop();
-    this->mainContext.stop();
+    this->mainContext->stop();
+}
 
-    this->tasksThread.join();
+void Node::advertise(unsigned short port) {
+    this->publisher = TcpPublisher::create(*this->ntwkContext, port);
+}
+
+void Node::subscribe(const Endpoint &endpoint, MsgTypeId msgType, MsgHandler msgHandler) {
+    auto &s = this->subscribers[endpoint];
+    if (!s) {
+        s = TcpSubscriber::create(*this->mainContext, *this->ntwkContext,
+                                  endpoint.first, endpoint.second);
+    }
+    s->subscribe(msgType, std::move(msgHandler));
+}
+
+void Node::publish(MsgTypeId msgTypeId, std::shared_ptr<flatbuffers::DetachedBuffer> msg) {
+    this->publisher->publish(msgTypeId, std::move(msg));
 }
 
 void Node::run() {
-    auto work = asio::make_work_guard(this->mainContext);
-    this->mainContext.run();
+    auto work = asio::make_work_guard(*this->mainContext);
+    this->mainContext->run();
 }
 
 void Node::runOnce() {
-    this->mainContext.poll_one();
-    this->mainContext.restart();
+    this->mainContext->poll();
+    this->mainContext->restart();
 }
 
 } // namespace ntwk
