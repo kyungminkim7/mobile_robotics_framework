b300d6c0254af5a117d24607c65d82a84bd1aa81
Your Name <you@example.com>
2021-11-12 17:12:24 +0000

    Added IMU enable sequence for BerryImu version 1 in constructor

--- src/BerryImu.cpp
+++ src/BerryImu.cpp
@@ -309,6 +309,21 @@ BerryImu::BerryImu(int device) {
             this->gyroAddr = LSM9DS0::GYR_ADDRESS;
             this->magAddr = LSM9DS0::MAG_ADDRESS;
 
+            // Enable gyro
+            this->selectDevice(this->gyroAddr);
+            this->writeByte(LSM9DS0::CTRL_REG1_G, 0b00001111); // Normal power mode, all axes enabled
+            this->writeByte(LSM9DS0::CTRL_REG4_G, 0b00110000); // Continuos update, 2000 dps full scale
+
+            // Enable accelerometer
+            this->selectDevice(this->accelAddr);
+            this->writeByte(LSM9DS0::CTRL_REG1_XM, 0b01100111); // xyz axis enabled, continuous update, 100 Hz
+            this->writeByte(LSM9DS0::CTRL_REG2_XM, 0b00100000); // +/- 16G full scale
+
+            // Enable magnetometer
+            this->selectDevice(this->magAddr);
+            this->writeByte(LSM9DS0::CTRL_REG5_XM, 0b11110000); // Temp enable, M data rate = 50Hz
+            this->writeByte(LSM9DS0::CTRL_REG6_XM, 0b11110000); // +/-12gauss
+            this->writeByte(LSM9DS0::CTRL_REG7_XM, 0b00000000); // Continuous-conversion mode
             
             return;
         }
